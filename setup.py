f1e41cb0bab570389057aaca20013e658d519e70
Cesar Augusto Sanchez <cesar.sanchez@behavox.com>
2023-04-02 01:33:41 -0400

    fix: update workflows

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 # pylint: disable=missing-module-docstring
 import setuptools
+import semantic_release
+import sys
+
+try:
+    from semantic_release import setup_hook
+    setup_hook(sys.argv)
+except ImportError:
+    pass
 
 if __name__ == "__main__":
     setuptools.setup(use_scm_version=True)
